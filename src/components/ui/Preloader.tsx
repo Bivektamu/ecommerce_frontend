64607d36a32ecd6eb42ff72746bae437ddadd6ab
Bivek Gurung <bivek.tamu@gmail.com>
2024-09-24 22:36:21 +1000

    removed framer motion code from preloader

--- src/components/ui/Preloader.tsx
+++ src/components/ui/Preloader.tsx
@@ -1,14 +1,8 @@
-import {motion} from 'framer-motion'
 const Preloader = () => {
   return (
-    <motion.section id='preloader' 
-    initial={{ opacity: 1 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 0.4 }}
-    className='fixed w-full h-screen bg-slate-200 animate-pulse top-0 left-0'>
-
-    </motion.section>
+    <section id='preloader'
+      className='fixed w-full h-screen bg-slate-200 animate-pulse top-0 left-0'>
+    </section>
   )
 }
 
